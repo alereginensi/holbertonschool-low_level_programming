b2dd113e52db30a352442ab0dc77ed134a252ef3
alereginensi <reginensia@gmail.com>
2022-02-03 09:44:23 -0800

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,9 @@ int main(void)
 	//sizeof evaluates the size of a variable
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
 	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of long long int: %zu byte\n", sizeof(longlongintType));
 
-	return 0;
+	return (0);
 }
