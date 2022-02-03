672d3945114da8457870349a184f5d79e129d4fb
alereginensi <reginensia@gmail.com>
2022-02-03 10:00:25 -0800

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 /**
- * main - dsajjs
- *
- * Return: Always 0 (Success)
- */
+ *  * main - print
+ *   * nothing
+ *    * Return: Always (0)
+ *    **/
 int main(void)
 {
-	int;
-	float;
-	long int;
-	char;
-	long long int;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of long int: %zu bytes\n", sizeof(longintType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
-	printf("Size of long long int: %zu byte\n", sizeof(longlongintType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
+ 
