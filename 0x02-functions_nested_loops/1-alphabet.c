85ad0c32cade9e641a67b28affd32f937289a099
alereginensi <reginensia@gmail.com>
2022-02-07 07:19:58 -0800

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,21 @@
 #include "main.h"
-/**
- *  * main - aaaaa
- *   *
- *    * Description: Same as above
- *     *
- *      * Return: Always 0 (Sucess)
- *       **/
-int main(void)
-{
-char    letter;
 
-	letter = 'a';
+/**
+ * print_alphabet - aaaaa
+ *
+ * Description: Same as above
+ *
+ * Return: Always 0 (Sucess)
+ **/
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
         while (letter <= 'z')
 	{
 		_putchar(letter);
 		letter++;
 	}
-	return(0);
+	_putchar('\n');
 }
