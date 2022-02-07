ebd094a807a7b4fc788d6e99d4e024f926125bbb
alereginensi <reginensia@gmail.com>
2022-02-07 08:34:05 -0800

    task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *  * int _islower(int letter) - aaaaa
- *   *
- *    * Description: Same as above
- *     *
- *      * Return: Always 0 (Sucess)
- *       **/
+ * int _islower(int letter) - aaaaa
+ *
+ * Description: Same as above
+ *
+ * Return: Always 0 (Sucess)
+ **/
 int _islower(int letter)
 {
 
