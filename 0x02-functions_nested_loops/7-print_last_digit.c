119c98fa6e079a91ef8a51b8102191fbd1fe2282
alereginensi <reginensia@gmail.com>
2022-02-07 10:15:35 -0800

    task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - jdsjsdj
+ *
+ * @i: sdjsadka
+ *
+ * Return: Always 0 (Sucess)
+ **/
+int print_last_digit(int i)
+{
+	int last = i % 10;
+
+	if (i < 0)
+
+
+	last = last * -1;
+
+	_putchar(last + '0');
+
+	return (last);
+}
+
