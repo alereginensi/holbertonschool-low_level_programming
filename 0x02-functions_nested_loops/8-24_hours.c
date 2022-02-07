4b2524fc0813aa86031e6a9da478fb670863b35c
alereginensi <reginensia@gmail.com>
2022-02-07 14:19:22 -0800

    task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,32 @@
 #include "main.h"
 /**
- * print_last_digit - jdsjsdj
+ * jack_bauer - jdsjsdj
  *
- * @i: sdjsadka
+ * @min: sdjsadka
+ * @hor: jdajsd
  *
  * Return: Always 0 (Sucess)
  **/
-int print_last_digit(int i)
+
+void jack_bauer(void)
 {
-	h <- seq(from=0, to=23)
-	m <- seq(from=0, to=59)
+	int hor;
+	int min;
 
-	h <- sprintf('%02d', h)
-	m <- sprintf('%02d', m)
+	for (hor = 0; hor < 24; hor++)
 
-	df <- data.frame(expand.grid(h, m))
-	df$times <- paste0(df$Var1, ':', df$Var2)
+	{
 
-	df <- df[order(df$times), ]
-	df$times
+		for (min = 0; min < 60; min++)
+
+		{
+
+			_putchar(hor / 10 + '0');
+			_putchar(hor % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+		}
+	}
 }
 
