77453dd91c09aeeb5966b1f4f55802c268f8df6b
alereginensi <reginensia@gmail.com>
2022-02-09 13:20:32 -0800

    task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - draws a straight line in the terminal.
+ * @size: num
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int num = 0;
+	int space;
+
+	if (size > 0)
+	{
+	while (num < size)
+	{
+		for (space = 0; space < size; space++)
+		{
+			_putchar('#');
+		}
+		_putchar('#');
+		_putchar('\n');
+		num++;
+	}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
