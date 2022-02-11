393dd32e3ac4110a6eb7bb222f12faaf2ae2aa66
alereginensi <reginensia@gmail.com>
2022-02-11 08:06:14 -0800

    task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @a: variable
+ * @b: variable
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = 98;
+}
