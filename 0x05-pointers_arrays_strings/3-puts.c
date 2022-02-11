130e435be453255d34803a1a8552e406c7cc73c9
alereginensi <reginensia@gmail.com>
2022-02-11 09:14:47 -0800

    task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: variable
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	fputs(str, stdout);
+}
