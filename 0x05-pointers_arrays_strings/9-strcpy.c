adc8068d6f8b31d2cfe842d6d60e785c44b841ec
alereginensi <reginensia@gmail.com>
2022-02-11 17:15:00 -0800

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function that copies the string
+ * @dest: variable
+ * @src: variable
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] < '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
