1e658860ed83382c532930bc760ce0b861f07f9b
alereginensi <reginensia@gmail.com>
2022-02-15 05:05:50 -0800

    task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: destination
+ * @src: source
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+
+	for (i = 0; dest[i] != '\0'; ++i)
+
+	for (j = 0; src[j] != '\0'; ++j, i++)
+	dest[i] = src[j];
+
+
+	dest[i] = '\0';
+
+	return (dest);
+}
