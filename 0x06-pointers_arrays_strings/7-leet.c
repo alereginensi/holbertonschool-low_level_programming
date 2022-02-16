a19209c6ce9734d9308f73e258743ea712c7412b
alereginensi <reginensia@gmail.com>
2022-02-16 05:43:19 -0800

    task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,10 +3,24 @@
 
 /**
  * leet - encodes a string into 1337.
- * @s: pointer
+ * @texto: string
  * Return: Always 0.
  */
 
-char *leet(char *s)
+char *leet(char *texto)
 {
+	char n[] = "4433007711";
+	char l[] = "aAeEoOtTlL";
+	int i, c;
+
+	for (i = 0; texto[i] != '\0'; i++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			if (texto[i] == l[c])
+				texto[i] = n[c];
+		}
+	}
+
+	return (texto);
 }
