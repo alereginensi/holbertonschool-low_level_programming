1fe225a3d20246772e0769df98862cd5d7930a77
alereginensi <reginensia@gmail.com>
2022-02-21 09:40:24 -0800

    task

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strspn - fills memory with a constant byte.
+* @s: the address of memory to print
+* @accept: the size of the memory to print
+*
+* Return: Nothing.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int y;
+
+	for (; *s != '\0'; s++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (*s == accept[y])
+			{
+				return (s);
+			}
+		}
+		if (*s == *accept)
+		{
+			return(s);
+		}
+	}
+	return ('\0');
+}
