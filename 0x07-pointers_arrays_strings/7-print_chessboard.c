738209901de509b7fe1d0fbb1eca64976fa7c980
alereginensi <reginensia@gmail.com>
2022-02-21 10:05:27 -0800

    task

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_chessboard - fills memory with a constant byte.
+* @a: the address of memory to print
+*
+* Return: Nothing.
+*/
+void print_chessboard(char (*a)[8])
+{
+
+	for (; *a <= 8; a++)
+	{
+		return (a);
+	}
+
+	return ('\0');
+}
