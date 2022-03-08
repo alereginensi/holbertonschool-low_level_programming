4e8f1cd954cf6e4f28518423a666305f089b246d
alereginensi <reginensia@gmail.com>
2022-03-08 07:34:33 -0800

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#include <stdio.h>
+/**
+ * struct dog - dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
