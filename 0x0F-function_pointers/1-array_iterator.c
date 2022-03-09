7494043bbd9224f76745f0be57e8662510e4f33b
alereginensi <reginensia@gmail.com>
2022-03-09 07:02:18 -0800

    task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (size = 0; array[size] != '\0'; size++)
+	{
+		action(*array);
+		array[size]++;
+	}
+}
