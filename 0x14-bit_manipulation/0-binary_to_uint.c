00d9d8e26693be7ff8c8081844ca9da7c0b94c2f
alereginensi <reginensia@gmail.com>
2022-03-24 10:32:26 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: variable
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	i++;
+
+	return (i);
+}
+
+/**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: char
  * Return: converted number
