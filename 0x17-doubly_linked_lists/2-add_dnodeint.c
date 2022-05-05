08fb21aab2643c2ef6dc8861f2ba7e2b1928a7d5
alereginensi <reginensia@gmail.com>
2022-05-05 10:40:39 -0700

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	new_node ->prev = NULL;
+
+	return (new_node);
+}
