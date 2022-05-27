510171a0bdbecdf886c0d1f0f6db716df543d294
alereginensi <reginensia@gmail.com>
2022-05-26 19:59:43 -0700

    task 0  try

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ *
+ *
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int x = 0;
+	hash_table_t *hash_t = NULL;
+
+	hash_t = malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
+		return (NULL);
+
+	hash_t->array = malloc(size * sizeof(hash_node_t *));
+
+	if (size == 0)
+		return (NULL);
+	hash_t->size = size;
+
+	if (hash_t->array == NULL)
+	{
+		free(hash_t);
+		return (NULL);
+	}
+
+	while (x < size)
+	{
+		hash_t->array[x] = NULL;
+		x++;
+	}
+	return (hash_t);
+}
