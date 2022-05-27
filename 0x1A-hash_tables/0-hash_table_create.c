04d63e4d7934a51d45411183b99ad6b428e84a59
alereginensi <reginensia@gmail.com>
2022-05-26 20:02:22 -0700

    betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
+ * hash_table_create - function that creates a hash table.
+ * @size: size
+ * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
